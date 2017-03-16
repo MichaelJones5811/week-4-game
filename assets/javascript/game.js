4adbf15aa47aa1bc94a73789216dd224cf2bdc25
michael jones <michael jones>
2017-03-16 17:55:36 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function(){
+
 
 alert("hello world");
 var wins = 0;
@@ -11,64 +11,69 @@ var yellowValue =  Math.floor(Math.random() * 12) + 1;
 var greenValue =  Math.floor(Math.random() * 12) + 1;  
 var userScore= 0;
 
+$(document).ready(function(){
 
 		
-		
-function resetValues() {
-		
-		 $("#randomNumber").html(randomNumber);
-		 // redValue = Math.floor(Math.random() * 12) + 1;  
-		 // blueValue = Math.floor(Math.random() * 12) + 1;  
-		 // yellowValue =  Math.floor(Math.random() * 12) + 1;  
-		 // greenValue =  Math.floor(Math.random() * 12) + 1; 
-		 userScore = 0; 
-		 alert(randomNumber);
-
-}
-
-function myFunction(){
-    $("#randomNumber").html(randomNumber);
-  
-	   $("#rc").on("click",function(){
-	   		userScore= userScore + (redValue);
-	   		$("#sampleWords").html(userScore);
-	   		checkNumber();
-	   	});
-	    $("#bc").on("click",function(){
-	   		userScore = userScore + (blueValue);
-	   		$("#sampleWords").html(userScore);
-	   		checkNumber();
-	   	});
-
-	   	 $("#yc").on("click",function(){
-	   		userScore = userScore + (yellowValue);
-	   		$("#sampleWords").html(userScore);
-	   		checkNumber();
-	   	});
-
-	   	  $("#gc").on("click",function(){
-	   		userScore = userScore + (greenValue);
-	   		$("#sampleWords").html(userScore);
-	   		checkNumber();
-	   	});
-	   	// checkNumber();
-	}
-	myFunction();
-	
-	function checkNumber(){
-		if(userScore === randomNumber){
-			wins++;
+// function to reset number for some of the variables	
+	function resetValues() {
 			
-			$("#wins").html(wins),
-			resetValues();
-		}
-		else if (userScore > randomNumber){
-			alert("you lost");
-			loses++;
-			$("#loses").html(loses);
-			resetValues();
-		}
+			 this.randomNumber = Math.floor(Math.random() * 120)+19;
+			 $("#randomNumber").html(randomNumber);	
+			 this.redValue = Math.floor(Math.random() * 12) + 1;; 
+			 this.blueValue = Math.floor(Math.random() * 12) + 1;;  
+			 this.yellowValue = Math.floor(Math.random() * 12) + 1;; 
+			 this.greenValue =  Math.floor(Math.random() * 12) + 1;; 
+			 this.userScore = 0; 
+			 $("#sampleWords").html(userScore);
+		
 	}
-// reset the random number of the varible
+// checks and compares varible values
+	function checkNumber(){
+			if(userScore === randomNumber){
+				wins++;
+				
+				$("#wins").html(wins);
+				resetValues();
+			}
+			else if (userScore > randomNumber){
+				alert("you lost");
+				loses++;
+				$("#loses").html(loses);
+				resetValues();	
+			}
+		}
+	// plays the game.
+	function myFunction(){
+	    $("#randomNumber").html(randomNumber);
+	  
+		   $("#rc").on("click",function(){
+		   		userScore += (redValue);
+		   		$("#sampleWords").html(userScore);
+		   		checkNumber();
+		   	});
+		    $("#bc").on("click",function(){
+		   		userScore += (blueValue);
+		   		$("#sampleWords").html(userScore);
+		   		checkNumber();
+		   	});
+
+		   	 $("#yc").on("click",function(){
+		   		userScore +=(yellowValue);
+		   		$("#sampleWords").html(userScore);
+		   		checkNumber();
+		   	});
+
+		   	  $("#gc").on("click",function(){
+		   		userScore +=(greenValue);
+		   		$("#sampleWords").html(userScore);
+		   		checkNumber();
+		   	});
+		   	
+		}
+		myFunction();
+		
+		
+	
+
 
 });
